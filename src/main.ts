dd3eac4a9c4967b6420cce7884e6f75a1b447180
Francisco Parra Penagos <francisco.parra.penagos@gmail.com>
2025-08-04 20:56:26 -0500

    ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './interceptors/timeout/timeout.interceptor';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
-import { ValidationPipe } from './pipes/validation/validation.pipe';
+// import { ValidationPipe } from './pipes/validation/validation.pipe';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { ValidationPipe } from '@nestjs/common';
 
 //The bootstrap method create the NestApplication instance and launch the application
 async function bootstrap() {
@@ -14,7 +15,20 @@ async function bootstrap() {
   //We can register globals filters with the useGlobalFilters method
   app.useGlobalFilters(new HttpExceptionFilter());
   //We can register globals pipes with the useGlobalPipes method
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
+
+  //ValidationPipe uses the libraries class-validator and class-transformer to make validations to the body of the incoming request
+  //For this we create the DTO's and add validations to this classes
+  //With witheList we can filter the properties that not are includeds in the dto
+  //With forbidNonWhitelisted we can handle an error wen the incoming request includes a prop who is not included in the dto
+  //With transform we can auto parse the incoming querys, params, and body props to theirs typed values
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   //We can register globals guards with the useGlobalGuards method
   app.useGlobalGuards(new AuthGuard());
   //this is like the listen of express
